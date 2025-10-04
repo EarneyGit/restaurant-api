7a301e7413b034d45f3985f36352112ef9f3a337
Jaya Suriyan <jayasuriyan14india@gmail.com>
2025-10-04 14:29:40 +0530

    Update CORS configuration to use allowedOrigins from environment variable

--- src/server.js
+++ src/server.js
@@ -54,6 +54,8 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000";
 
+const allowedOrigins = JSON.parse(process.env.ALLOWED_ORIGINS || '["*"]')
+
 // Initialize Socket.IO
 const io = initSocket(server);
 
@@ -63,7 +65,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
